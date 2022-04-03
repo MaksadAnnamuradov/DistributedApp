ac6b9b883a0ee33812315d3fcb1562b7ab8c832b
Maksad Annamuradov <maksad.annamuradow98@gmail.com>
2022-04-03 00:21:02 -0600

    worked on worker and server interaction

--- server/Program.cs
+++ server/Program.cs
@@ -10,11 +10,12 @@ class Server
     static UTF8Encoding encoding = new UTF8Encoding();
     static readonly ConcurrentDictionary<long, List<long>> CacheDict =
             new ConcurrentDictionary<long, List<long>>();
+    static Dictionary<string, string> workersList = new Dictionary<string, string>();
     static void Main()
     {
         while (true)
         {
-            // start a thread to respond
+            //start a thread to respond
             var clientTask = Task.Run(() =>
             {
 
@@ -50,7 +51,18 @@ class Server
                     else //was not added
                     {
                         Console.WriteLine($"Calling Prime factors and adding {requestNum} to cache");
-                        answer = GetPrimeFactors(requestNum);
+
+                        var freeWorkers = workersList.Where(x => x.Value == "free").ToList();
+                        Console.WriteLine($"Free workers: {freeWorkers.Count}");
+
+
+                        Console.WriteLine("Sending {0} to {1}", requestNum, freeWorkers[0].Key);
+                        byte[] workerData = encoding.GetBytes(requestString);
+
+                        UdpClient toWorker = new UdpClient();
+
+                        toWorker.Send(workerData, workerData.Length, requester);
+
                         CacheDict.GetOrAdd(requestString.GetHashCode(), answer);
                     }
 
@@ -86,35 +98,40 @@ class Server
                 byte[] requestData = listener.Receive(ref requester);
                 string requestString = encoding.GetString(requestData);
 
-                long requestNum = long.Parse(requestString);
+                if(requestString.Contains("free"))
+                {
+                    Console.WriteLine("Received {0} from {1}", requestString, requester);
+                    workersList.Add(requester.ToString(), "free");
+                }
+                else
+                {
+                    Console.WriteLine("Received {0} from {1}", requestString, requester);
+                    workersList.Add(requester.ToString(), "busy");
+                }
+
+                //long requestNum = long.Parse(requestString);
 
                 Console.WriteLine("Received {0} from {1}", requestString, requester);
 
 
-                List<long> answer = new List<long>();
+                // List<long> answer = new List<long>();
 
-                if (CacheDict.ContainsKey(requestString.GetHashCode()))
-                {
-                    Console.WriteLine($"Number was already in cache for {requestNum}");
-                    answer = CacheDict.GetOrAdd(requestString.GetHashCode(), answer);
-                }
-                else //was not added
-                {
-                    Console.WriteLine($"Calling Prime factors and adding {requestNum} to cache");
-                    answer = GetPrimeFactors(requestNum);
-                    CacheDict.GetOrAdd(requestString.GetHashCode(), answer);
-                }
+                // if (CacheDict.ContainsKey(requestString.GetHashCode()))
+                // {
+                //     Console.WriteLine($"Number was already in cache for {requestNum}");
+                //     answer = CacheDict.GetOrAdd(requestString.GetHashCode(), answer);
+                // }
+                // else //was not added
+                // {
+                //     Console.WriteLine($"Calling Prime factors and adding {requestNum} to cache");
+                //     answer = GetPrimeFactors(requestNum);
+                //     CacheDict.GetOrAdd(requestString.GetHashCode(), answer);
+                // }
 
-                Console.WriteLine("Starting task at time " + sw.Elapsed);
+                //Console.WriteLine("Starting task at time " + sw.Elapsed);
 
-                string response = String.Join(',', answer.Select(p => p.ToString()));
+                //string response = String.Join(',', answer.Select(p => p.ToString()));
 
-                Console.WriteLine("Sending {0} to {1}", response, requester);
-                byte[] responseData = encoding.GetBytes(response);
-
-                UdpClient toClient = new UdpClient();
-
-                toClient.Send(responseData, responseData.Length, requester);
             });
 
             clientTask.Wait();
@@ -122,31 +139,6 @@ class Server
         }
     }
 
-    static List<long> GetPrimeFactors(long product)
-    {
-        var sw = Stopwatch.StartNew();
-        Console.WriteLine("Getting Prime factors for {0}", product);
-
-        if (product <= 1)
-        {
-            throw new ArgumentException();
-        }
-        List<long> factors = new List<long>();
-        // divide out factors in increasing order until we get to 1
-        while (product > 1)
-        {
-            for (long i = 2; i <= product; i++)
-            {
-                if (product % i == 0)
-                {
-                    product /= i;
-                    factors.Add(i);
-                    break;
-                }
-            }
-        }
-        Console.WriteLine("Prime factors finished after " + sw.Elapsed);
-        return factors;
-    }
+   
 
 }
--- worker/Program.cs
+++ worker/Program.cs
@@ -1,2 +1,97 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Diagnostics;
+using System;
+
+
+class Worker
+{
+
+    static UTF8Encoding encoding = new UTF8Encoding();
+    static Stopwatch sw = Stopwatch.StartNew();
+
+    // create a list of states
+    static string workerState = "free";
+    
+    static void Main()
+    {
+        while (true)
+        {
+
+            //wait for a message from the server
+            var from = new IPEndPoint(0, 0);
+            byte[] recvBuffer = udpClient.Receive(ref from);
+            string message = encoding.GetString(recvBuffer);
+            Console.WriteLine("{0} received from {1}", message, from);
+            
+            int PORT = 6555;
+            UdpClient udpClient = new UdpClient();
+
+
+            if(workerState == "free")
+            {
+                workerState = "busy";
+
+                var sendData = encoding.GetBytes($"free");
+                Console.WriteLine("Sending {0} state to the server", sendData);
+                udpClient.Send(sendData, data.ToString().Length, "255.255.255.255", PORT);
+
+                //wait for a message from the server
+                byte[] recvBuffer = udpClient.Receive(ref from);
+                string message = encoding.GetString(recvBuffer);
+                Console.WriteLine("{0} received from {1}", message, from);
+                
+                //call getPrimeFactors
+                Console.WriteLine($"Calling Prime factors");
+                var answer = GetPrimeFactors(requestNum);
+
+                string response = String.Join(',', answer.Select(p => p.ToString()));
+
+                Console.WriteLine("Sending {0} to {1}", response, from);
+                byte[] responseData = encoding.GetBytes(response);
+
+                UdpClient toClient = new UdpClient();
+
+                toClient.Send(responseData, responseData.Length, from);
+
+                workerState = "free";
+            }else{
+                string response = workerState;
+
+                Console.WriteLine("Sending {0} to {1}", response, from);
+                byte[] responseData = encoding.GetBytes(response);
+
+                UdpClient toClient = new UdpClient();
+                toClient.Send(responseData, responseData.Length, from);
+            }
+        }
+    }
+
+     static List<long> GetPrimeFactors(long product)
+    {
+        var sw = Stopwatch.StartNew();
+        Console.WriteLine("Getting Prime factors for {0}", product);
+
+        if (product <= 1)
+        {
+            throw new ArgumentException();
+        }
+        List<long> factors = new List<long>();
+        // divide out factors in increasing order until we get to 1
+        while (product > 1)
+        {
+            for (long i = 2; i <= product; i++)
+            {
+                if (product % i == 0)
+                {
+                    product /= i;
+                    factors.Add(i);
+                    break;
+                }
+            }
+        }
+        Console.WriteLine("Prime factors finished after " + sw.Elapsed);
+        return factors;
+    }
+}
