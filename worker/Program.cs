4835445e61d8832622f2d9858b993080708d4a5b
Maksad Annamuradov <maksad.annamuradow98@gmail.com>
2022-04-10 16:09:17 -0600

    added all necc files

--- worker/Program.cs
+++ worker/Program.cs
@@ -22,7 +22,6 @@ class Worker
 
         var sendData = encoding.GetBytes("free");
         Console.WriteLine("Sending {0} state to the server", workerState);
-
         
         workerClient.Send(sendData, sendData.Length, "127.0.0.1", SERVER_PORT);
 
